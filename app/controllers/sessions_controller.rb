7175e9b86e9aa81917a9338c9398541e94dfdeb0
Alex Kln <kalininmalinin@gmail.com>
2020-05-15 15:49:52 +0300

    fix login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.authenticate(params[:email], params[:password])
-    if @user.present?
+    if user.present?
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Вы успешно залогинились'
     else
