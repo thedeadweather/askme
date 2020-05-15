d26cf884cf815c73825259964ff61af799f0a67c
Alex Kln <kalininmalinin@gmail.com>
2020-05-15 20:01:22 +0300

    edit migrations

--- db/migrate/20200515133509_add_profile_color_to_user.rb
+++ db/migrate/20200515133509_add_profile_color_to_user.rb
@@ -1,5 +1,5 @@
 class AddProfileColorToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :profile_color, :color
+    add_column :users, :profile_color, :string
   end
 end
--- db/migrate/20200515164011_change_color_to_be_string_in_user.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColorToBeStringInUser < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :profile_color, :string
-  end
-end
