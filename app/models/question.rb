e36bebb3e98240527f273a274368aa80e9dd6855
Alex Kln <kalininmalinin@gmail.com>
2020-06-12 22:53:26 +0300

    edit delete_all dependency for hashtags collection in question controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
   has_many :hashtag_questions, dependent: :destroy
-  has_many :hashtags, through: :hashtag_questions
+  has_many :hashtags, through: :hashtag_questions, dependent: :nullify
 
   validates :text, presence: true
   # проверка макс длины текста
